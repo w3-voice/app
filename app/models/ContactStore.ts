ba21472743e49e17c4eac3dea6880025e2551dc8
farhoud <farhoud.m7@gmail.com>
2023-01-04 17:33:35 +0330

    manage form

--- app/models/ContactStore.ts
+++ app/models/ContactStore.ts
@@ -2,29 +2,55 @@ import { flow, Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { ContactModel } from "./Contact"
 import { createNewContactDefaultModel } from "./NewContact"
 import { api } from "../services/core"
-import { load } from "../utils/storage"
 /**
  * Model description here for TypeScript hints.
  */
 export const ContactStoreModel = types
   .model("ContactStore")
   .props({
-    list: types.array(ContactModel),
-    form: createNewContactDefaultModel()
+    contacts: types.map(ContactModel),
+    form: createNewContactDefaultModel(),
+    hasMore: types.maybe(types.boolean)
   })
-  .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .views((self) => ({
+    get list() {
+      return [...self.contacts.values()].sort((a, b) => a.name.localeCompare(b.name) )
+    },
+    has(id) {
+      return self.contacts.has(id)
+    }
+  })) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions((self) => ({
-    load: flow(function* loadContact() {
-      const list = yield api.beeCore.contact.list(0, 50)
-      self.list.replace(list)
+    load: flow(function* loadContact(ids?:string[]) {
+      if(ids != undefined){
+        const cons = []
+        for(let id of ids){
+          const exist = self.contacts.has(id)
+          if(!exist){
+            const con = yield api.beeCore.contact.get(id)
+            cons.push(con)
+            self.contacts.put(con)
+          }
+        }
+        
+      } else {
+        const res = yield api.beeCore.contact.list(0, 50)
+        self.hasMore = res.length == 50
+        res.forEach((con)=>{self.contacts.put(con)})
+      }
     }),
     add: flow(function* addContacts() {
       self.form.saving = true;
       const newContact = { _id:self.form._id, name:self.form.name }
       try {
-        yield api.beeCore.contact.add(newContact)
-        self.list.push(newContact)
-        self.form.done = true
+        const res = yield api.beeCore.contact.add(newContact)
+        if(res){
+          self.contacts.put(newContact)
+        }else{
+          self.form.err = "can not add contact"
+        }
+        
+        self.form.done = res
       } catch (error) {
         self.form.err = error
         console.log("failed to create new contract", error)
