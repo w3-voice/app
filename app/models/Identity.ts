a67816773716e5a50b8cf2468edbfcba40287296
farhoud <farhoud.m7@gmail.com>
2023-01-04 17:34:18 +0330

    identity most expos user of type Contact

--- app/models/Identity.ts
+++ app/models/Identity.ts
@@ -8,37 +8,28 @@ import { ContactModel } from "./Contact"
 export const IdentityModel = types
   .model("Identity")
   .props({
-    _id: types.maybeNull(types.string),
-    name: types.string,
+    user: types.maybeNull(ContactModel),
+    form: types.string,
     isLoggedIn: types.boolean
   })
   .views((self) => ({
-    get user(){
-      if(!self._id || !self.name ){
-        return null
-      }
-      return {_id:self._id, name:self.name}
-    }
   })) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions((self) => {
     const newIdentity = flow(function* newIdentity(name: string) {
       let idObj = yield api.beeCore.identity.create(name)
-      self._id = idObj._id
-      self.name = idObj.name 
+      self.user = idObj
       self.isLoggedIn = true
     })
     const loadIdentity = flow(function* load() {
       try {
         let hasId = yield api.beeCore.identity.has()
         if (!hasId) {
-          self._id = null
-          self.name = ""
+          self.user = null
           self.isLoggedIn = false
         }
         else {
           let idObj = yield api.beeCore.identity.get()
-          self._id = idObj._id
-          self.name = idObj.name 
+          self.user = idObj
           self.isLoggedIn = true
         }
       } catch (error) {
@@ -46,17 +37,17 @@ export const IdentityModel = types
       }
 
     })
-    const setName = (name) => {
-      self.name = name
+    const setForm = (name) => {
+      self.form = name
     }
     return {
       loadIdentity,
       newIdentity,
-      setName
+      setForm
     }
   }) // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export interface Identity extends Instance<typeof IdentityModel> { }
 export interface IdentitySnapshotOut extends SnapshotOut<typeof IdentityModel> { }
 export interface IdentitySnapshotIn extends SnapshotIn<typeof IdentityModel> { }
-export const createIdentityDefaultModel = () => types.optional(IdentityModel, {isLoggedIn:false, name:""})
+export const createIdentityDefaultModel = () => types.optional(IdentityModel, {isLoggedIn:false, form:""})
