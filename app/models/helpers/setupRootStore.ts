25f79fa99ef68907487ff00304ffc8faca5e3738
farhoud <farhoud.m7@gmail.com>
2023-01-12 16:33:47 +0330

    remove subscribe from loading

--- app/models/helpers/setupRootStore.ts
+++ app/models/helpers/setupRootStore.ts
@@ -32,9 +32,7 @@ export async function setupRootStore(rootStore: RootStore) {
     if (!_subscribed){
       const bonded = await api.beeCore.bindService()
       await rootStore.identityStore.loadIdentity()
-      await rootStore.chatStore.loadChatList()
       await rootStore.permissionStore.load()
-      coreSync(rootStore.chatStore)
       _subscribed = true;
     }
    
